b3c11450c66ed95b82d0e8360bb71a2c9c0da92f
Hudson <dhudsmith@gmail.com>
2023-08-17 17:02:24 -0400

    remove unneccesary deployment code

--- apptainer.sh
+++
@@ -1,8 +0,0 @@
-SCRIPT_DIR="/home/dane2/Code/dcm2vid"
-cd $SCRIPT_DIR
-module load cuda/12.0.1-gcc
-ls
-apptainer exec --nv --bind \
-    /project/rcde,/scratch/$USER,/zfs/wficai \
-    /software/containers/pytorch/pytorch-ffmpeg_latest.sif \
-    /bin/bash
